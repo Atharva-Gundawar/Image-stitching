6cf9e6fb3932cb4586901623c7ed93269ebda1f6
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-01 23:51:36 +0530

    Clean: main.py

--- main.py
+++ main.py
@@ -8,14 +8,10 @@ import argpharser
 feature_extractor = 'orb' # one of 'sift', 'surf', 'brisk', 'orb'
 feature_matching = 'bf'
 
-# read images and transform them to grayscale
-# Make sure that the train image is the image that will be transformed
 trainImg = imageio.imread('http://www.ic.unicamp.br/~helio/imagens_registro/foto1A.jpg')
 trainImg_gray = cv2.cvtColor(trainImg, cv2.COLOR_RGB2GRAY)
 
 queryImg = imageio.imread('http://www.ic.unicamp.br/~helio/imagens_registro/foto1B.jpg')
-# Opencv defines the color channel in the order BGR. 
-# Transform it to RGB to be compatible to matplotlib
 queryImg_gray = cv2.cvtColor(queryImg, cv2.COLOR_RGB2GRAY)
 
 fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, constrained_layout=False, figsize=(16,9))
@@ -35,7 +31,6 @@ def detectAndDescribe(image, method=None):
     
     assert method is not None, "You need to define a feature detection method. Values are: 'sift', 'surf'"
     
-    # detect and extract features from the image
     if method == 'sift':
         descriptor = cv2.xfeatures2d.SIFT_create()
     elif method == 'surf':
@@ -45,7 +40,6 @@ def detectAndDescribe(image, method=None):
     elif method == 'orb':
         descriptor = cv2.ORB_create()
         
-    # get keypoints and descriptors
     (kps, features) = descriptor.detectAndCompute(image, None)
     
     return (kps, features)
@@ -53,7 +47,6 @@ def detectAndDescribe(image, method=None):
 kpsA, featuresA = detectAndDescribe(trainImg_gray, method=feature_extractor)
 kpsB, featuresB = detectAndDescribe(queryImg_gray, method=feature_extractor)
 
-# display the keypoints and features detected on both images
 fig, (ax1,ax2) = plt.subplots(nrows=1, ncols=2, figsize=(20,8), constrained_layout=False)
 ax1.imshow(cv2.drawKeypoints(trainImg_gray,kpsA,None,color=(0,255,0)))
 ax1.set_xlabel("(a)", fontsize=14)
@@ -75,26 +68,18 @@ def createMatcher(method,crossCheck):
 def matchKeyPointsBF(featuresA, featuresB, method):
     bf = createMatcher(method, crossCheck=True)
         
-    # Match descriptors.
     best_matches = bf.match(featuresA,featuresB)
-    
-    # Sort the features in order of distance.
-    # The points with small distance (more similarity) are ordered first in the vector
     rawMatches = sorted(best_matches, key = lambda x:x.distance)
     print("Raw matches (Brute force):", len(rawMatches))
     return rawMatches
 
 def matchKeyPointsKNN(featuresA, featuresB, ratio, method):
     bf = createMatcher(method, crossCheck=False)
-    # compute the raw matches and initialize the list of actual matches
     rawMatches = bf.knnMatch(featuresA, featuresB, 2)
     print("Raw matches (knn):", len(rawMatches))
     matches = []
 
-    # loop over the raw matches
     for m,n in rawMatches:
-        # ensure the distance is within a certain ratio of each
-        # other (i.e. Lowe's ratio test)
         if m.distance < n.distance * ratio:
             matches.append(m)
     return matches
@@ -118,17 +103,14 @@ plt.show()
 
 
 def getHomography(kpsA, kpsB, featuresA, featuresB, matches, reprojThresh):
-    # convert the keypoints to numpy arrays
     kpsA = np.float32([kp.pt for kp in kpsA])
     kpsB = np.float32([kp.pt for kp in kpsB])
     
     if len(matches) > 4:
 
-        # construct the two sets of points
         ptsA = np.float32([kpsA[m.queryIdx] for m in matches])
         ptsB = np.float32([kpsB[m.trainIdx] for m in matches])
         
-        # estimate the homography between the sets of points
         (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC,
             reprojThresh)
 
@@ -142,7 +124,6 @@ if M is None:
 (matches, H, status) = M
 print(H)
 
-# Apply panorama correction
 width = trainImg.shape[1] + queryImg.shape[1]
 height = trainImg.shape[0] + queryImg.shape[0]
 
@@ -155,23 +136,17 @@ plt.imshow(result)
 plt.axis('off')
 plt.show()
 
-# transform the panorama image to grayscale and threshold it 
 gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
 
-# Finds contours from the binary image
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 
-# get the maximum contour area
 c = max(cnts, key=cv2.contourArea)
 
-# get a bbox from the contour area
 (x, y, w, h) = cv2.boundingRect(c)
 
-# crop the image to the bbox coordinates
 result = result[y:y + h, x:x + w]
 
-# show the cropped image
 plt.figure(figsize=(20,10))
 plt.imshow(result)
